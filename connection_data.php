72697e96009218de32d898c352141d95bbabf117
Sajid Hussain <sajid6207116@gmail.com>
2024-01-04 23:35:00 +0500

    database connected

--- connection_data.php
+++ connection_data.php
@@ -1,10 +1,20 @@
 <?php
 
-echo $con_ID = $_POST['connectionID'];
-echo $con_customerName = $_POST['customer_name'];
-echo $con_Type = $_POST['connection_type'];
-echo $con_Date = $_POST['connection_date'];
-echo $con_occupation = $_POST['occupation'];
-echo $con_address = $_POST['address'];
+    $con_ID = $_POST['connectionID'];
+    $con_customerName = $_POST['customer_name'];
+    $con_Type = $_POST['connection_type'];
+    $con_Date = $_POST['connection_date'];
+    $con_occupation = $_POST['occupation'];
+    $con_address = $_POST['address'];
 
+
+    $conn = mysqli_connect("localhost", "root", "", "sui_gas") or die("Connection Failed");
+
+    $sql = "INSERT INTO add_connection(10_dcid,customer_name,connection_type,connection_date,occupation,address) VALUES('{$con_ID}','{$con_customerName}','{$con_Type}','{$con_Date}','{$con_occupation}','{$con_address}')";
+   
+    $result = mysqli_query($conn, $sql);
+
+    header("Location: http://localhost/suigasbillingsystem/view_customer.php");
+
+    mysqli_close($conn);
 ?>
