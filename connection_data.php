b7c5e73d5c0320275aa19652c2f3874f59a8c2e8
Sajid Hussain <sajid6207116@gmail.com>
2024-01-04 23:28:06 +0500

    problem fixed

---
+++ connection_data.php
@@ -0,0 +1,10 @@
+<?php
+
+echo $con_ID = $_POST['connectionID'];
+echo $con_customerName = $_POST['customer_name'];
+echo $con_Type = $_POST['connection_type'];
+echo $con_Date = $_POST['connection_date'];
+echo $con_occupation = $_POST['occupation'];
+echo $con_address = $_POST['address'];
+
+?>
