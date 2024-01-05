c25e47df58194b50b1c16e93a24bbd48e536581b
Sajid Hussain <sajid6207116@gmail.com>
2024-01-06 00:58:19 +0500

    name changed

--- no-index.php
+++ no-index.php
@@ -11,17 +11,8 @@
       crossorigin="anonymous"
     />
   </head>
-  <body style="padding: 100px;">
-    <h1>User Pannel</h1>
-    <form action="user_pannel.php" method="post">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Enter 10-Digit Consumer Number</label>
-          <input type="number" name="consumer_number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <button type="submit" value="search" class="btn btn-primary">Submit</button>
-      </form>
-
-
+  <body>
+    <h1>Welcome! Sui Gas Billing System</h1>
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
